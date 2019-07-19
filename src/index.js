7913dad18fa7a9d57a26162b860471e62af920da
splean101 <101splean@gmail.com>
2019-07-19 11:25:22 +0300

    init

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class AnimationText extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { };
+        
+    }
+    render() {
+        return 
+    }
+};
+
+ReactDOM.render(<AnimationText text='bla-bla-bla' />, 
+document.getElementById('div1'))
