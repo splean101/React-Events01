2ed6828eac5e8a85bf48e12af4905ab0a29a3cda
splean101 <101splean@gmail.com>
2019-07-19 11:38:24 +0300

    fin

--- src/index.js
+++ src/index.js
@@ -4,13 +4,26 @@ import ReactDOM from 'react-dom';
 class AnimationText extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { };
-        
+        this.state = { num: 0 };
+        this.plus = this.plus.bind(this);
+        this.minus = this.minus.bind(this);
+    }
+    plus() {
+        this.setState((prevState) => ({ num: ++prevState.num }));
+    }
+    minus() {
+        this.setState((prevState) => ({ num: --prevState.num }));
     }
     render() {
-        return 
+        return (
+            <> 
+            <button type='button' onClick={this.minus}>-</button>
+            <b> {this.state.num} </b>
+            <button type='button' onClick={this.plus}>+</button>
+            </>
+        )
     }
 };
 
-ReactDOM.render(<AnimationText text='bla-bla-bla' />, 
-document.getElementById('div1'))
+ReactDOM.render(<AnimationText />,
+    document.getElementById('div1'))
